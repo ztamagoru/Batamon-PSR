b8af785e149a1f3593936f52e3f1f670ab0d7065
tamagoru <marushannnn@outlook.com>
2023-03-14 09:18:27 -0300

    Add project files.

---
+++ batamon - unit test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using 
+
+namespace batamon___unit_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ batamon/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace batamon
+{
+    public class Batamon
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    
+        public static int batamon(List<int> hechicera, List<int> rival)
+        {
+            
+            return 0;
+        }
+    }
+}  

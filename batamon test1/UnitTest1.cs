fb58022a0f834be6e62bbefe98b0184af01f13df
tamagoru <marushannnn@outlook.com>
2023-03-14 18:26:30 -0300

    sending code to pc through git

--- batamon test1/UnitTest1.cs
+++ batamon test1/UnitTest1.cs
@@ -11,7 +11,40 @@ namespace batamon_test1
             var hechicera = new List<int>() { 1, 1 };
             var rival = new List<int>() { 1, 1 };
             var resultado = Batamon1.batamon(hechicera, rival);
-            Assert.AreEqual(0, resultado);
+            Assert.AreEqual(2, resultado);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Test2()
+        {
+            // cantidad de monstruos diferente
+            // excepcion esperada
+
+            var hechicera = new List<int>() { 1, 2, 4 };
+            var rival = new List<int>() { 1, 1 };
+            var resultado = Batamon1.batamon(hechicera, rival);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Test3()
+        {
+            // niveles fuera de rango
+            // excepcion esperada
+
+            var hechicera = new List<int>() { 0, 1 };
+            var rival = new List<int>() { 1, 3000009 };
+            var resultado = Batamon1.batamon(hechicera, rival);
+        }
+
+        [TestMethod]
+        public void Ejemplo()
+        {
+            var hechicera = new List<int>() { 8, 10, 5, 23, 10, 10, 2 };
+            var rival = new List<int>() { 5, 4, 13, 72, 12, 5, 10 };
+            var resultado = Batamon1.batamon(hechicera, rival);
+            Assert.AreEqual(5, resultado);
         }
     }
 }
--- batamon/Main.cs
+++ batamon/Main.cs
@@ -6,13 +6,93 @@ namespace batamon
     {
         public static void Main()
         {
-            Console.WriteLine("Hello, world!");
-        }
+            // El usuario ingresa la cantidad de enfrentamientos que habrá.
+            
+            var firstline = Console.ReadLine();
+            int N = int.Parse(firstline);
+
+            // Si la cantidad de enfrentamientos es menor a 1 o mayor a 200.000, tira 
+            // una excepción.
+
+            if (N < 1 || N > 200000)
+                throw new Exception("Error. Cantidad de enfrentamientos menor a 1 o mayor a 200.000.");
+
+            // El usuario ingresa los niveles de poder de los monstruos de la hechicera
+
+            var secondline = Console.ReadLine();
+            var unparsedhechicera = secondline.Split(' ');
+            var hechicera = new List<int>();
+            foreach (var unparsed in unparsedhechicera)
+            {
+                hechicera.Add(int.Parse(unparsed));
+            }
+
+            // El usuario ingresa los niveles de poder de los monstruos del rival.
+
+            var thirdline = Console.ReadLine();
+            var unparsedrival = thirdline.Split(' ');
+            var rival = new List<int>();
+            foreach (var unparsed in unparsedrival)
+            {
+                rival.Add(int.Parse(unparsed));
+            }
+
+            // Si la cantidad de niveles ingresados de los monstruos de la hechicera o
+            // del rival no es la misma que la cantidad de enfrentamientos, tira una
+            // excepción.
+
+            if (hechicera.Count != N || rival.Count != N)
+                throw new Exception("Error. Cantidad de niveles ingresados no coincide con el primer parámetro.");
+
+            // Envía los parámetros a la función "batamon" y muestra el resultado en pantalla.
+
+            Console.WriteLine("\nEnfrentamientos ganados:");
+            Console.WriteLine(batamon(hechicera, rival));
+            }
     
         public static int batamon(List<int> hechicera, List<int> rival)
         {
+            // Si 
+
+            if (hechicera.Count != rival.Count)
+                throw new Exception("Cantidad de niveles diferentes entre los monstruos de la hechicera y del rival.");
+
+            // Si el nivel de algun monstruo se encuentra fuera del rango, tira una
+            // excepcion
+
+            if (hechicera.Any(hechicera => hechicera < 1 || hechicera > 1000000) || 
+                rival.Any(rival => rival < 1 || rival > 1000000))
+                throw new Exception("Nivel de monstruos fuera de rango.");
+
+            // Inicializo el resultado a devolver
             
-            return 0;
+            int result = 0;
+
+            // Ordeno las listas de los niveles por orden ascendiente y descendiente.
+
+            hechicera.Sort();
+
+            rival.Sort();
+
+            foreach (var n in hechicera)
+                Debug.Write(n "");
+
+            Debug.WriteLine("");
+            foreach (var n in rival)
+                Debug.Write(n);
+
+//            rival.Reverse();
+
+            // En el caso de que el nivel del monstruo de la hechicera sea igual o mayor al nivel del
+            // monstruo del rival, la hechicera suma un punto. Al terminar, devuelve el resultado.
+
+            for (int i = 0; i < hechicera.Count; i++)
+            {
+                if (hechicera[i] >= rival[i])
+                    result++;
+            }
+
+            return result;
         }
     }
 }  
